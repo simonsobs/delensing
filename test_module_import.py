f7cd31b1606bf478e7f0d770a38acf20e5b68525
Toshiya Namikawa <namikawa.toshiya9@gmail.com>
2021-10-24 23:08:41 -0700

    published version 2021-10-20

---
+++ test_module_import.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import so_pysm_models
+#help(so_pysm_models)
+from mapsims import SONoiseSimulator, SOStandalonePrecomputedCMB, SOChannel, noise
+import os
+import sys
+print(sys.path)
+
+
+# In[ ]:
+
+
+
+
